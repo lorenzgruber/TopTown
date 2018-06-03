cd3a6a7d2d7a65a0ad3c9a162d5f7a37833e66ff
LMFRegru <36515140+LMFRegru@users.noreply.github.com>
2018-06-03 20:25:32 +0200

    :3

--- src/js/game/items.js
+++
@@ -1,7 +0,0 @@
-function Hat(name, type, description, img, ingameImg){
-  this.name = name;
-  this. type = type;
-  this.description = description;
-  this.img = img;
-  this.ingameImg = ingameImg;
-}
